f2fbdb7eb4a2da486896078467c1289df32a8ce3
KaiqueCarlos <kaique.silva49@fatec.gov.sp.br>
2023-11-22 20:12:07 -0300

    22/11/2023

--- index.js
+++ index.js
@@ -3,67 +3,59 @@ const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const Post = require('./models/Post');
-const session = require('express-session');
+//const session = require('express-session');
 
-//config 
+
 //template Engine
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
-// Body-Parser
 
 // Middleware para analisar dados JSON
 app.use(bodyParser.json());
 // Middleware para analisar dados de formulário (application/x-www-form-urlencoded)
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Seção de usuário
+//app.use(session({ secret: 'hghjhhjhk' }));
 
-// Rotas cadastro
-app.get('/', function (req, res) {
-   res.render('login');
+// Rotas para a página de login
+app.get('/', (req, res) => {
+  res.render('login');
 });
 
+// Rota para a página inicial (acessar apenas após realizar login)
+app.get('/paginainicial', (req, res) => {
+  res.render('pagina_inicial');
+});
 
-app.post('/login', async (req, res) => {
-    const { email, senha } = req.body;
-  
-    try{
-      const user = await Post.findOne({ where: { email, senha } });
-  
-      if (user) {
-        res.redirect('/redirect_inicial');
-      } else {
-        res.send('Credenciais inválidas');
-      }
-    } catch (error) {
-      console.error('Erro no login:', error);
-      res.status(500).send('Erro interno');
-    }
-  });
-
-  app.get('/redirect_inicial', function(req, res){
-    res.render('pagina_inicial');
-  })
-  
-
-
+// Rota para cadastrar novo usuário (página de login)
 app.post('/add', function (req, res) {
-    Post.create({
-        nome: req.body.nome,
-        sobrenome: req.body.sobrenome,
-        email: req.body.email,
-        senha: req.body.senha,
-        cidade: req.body.cidade,
-        telefone: req.body.telefone
-    })
+  Post.create({
+    nome: req.body.nome,
+    sobrenome: req.body.sobrenome,
+    email: req.body.email,
+    senha: req.body.senha,
+    cidade: req.body.cidade,
+    telefone: req.body.telefone
+  })
+});
+
+// Rota para cadastrar animais (página inicial)
+app.post('/addAnimal', function (req, res) {
+  Post.create({
+    nome: req.body.nome,
+    especie: req.body.especie,
+    raca: req.body.raca,
+    sexo: req.body.sexo,
+    idade: req.body.idade
+  })
 });
 
 
 
 
 
-
-
+// Porta para acessar o página utilizando um servidor local
 app.listen(2000, function () {
-    console.log("Servidor rodando http://localhost:2000 ");
+  console.log("Servidor rodando http://localhost:2000 ");
 });
--- models/db.js
+++ models/db.js
@@ -5,6 +5,15 @@ const sequelize = new Sequelize('pisegundo', 'root', '105117Kc', {
     dialect: 'mysql'
 });
 
+sequelize.authenticate()
+.then(function(){
+    console.log("Conectado com sucesso ao banco de dados.");
+
+}).catch(function(){
+    console.log("Erro na conexão com o banco de dados.");
+
+});
+
 module.exports = {
     
     Sequelize: Sequelize,
